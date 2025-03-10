5c6bd0c7189610191d8068559083e6130a42b97b
KTykhomyrova <tykhomyrovak@gmail.com>
2025-03-10 11:19:21 +0100

    Add files via upload

---
+++ Tykhomyrova/10.03_9.4_konstruktory/10.03_9.4_konstruktory/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._03_9._4_konstruktory {
+
+    public enum Grade {
+        FirstGrade,
+        SecondGrade,
+        ThirdGrade,
+        FourthGrade,
+        FifthGrade
+         
+    }
+
+    public class School {
+        //prywatne pola
+        private string _studentName;
+        private Grade _studentGrade;
+        private DateOnly _birthDate;
+        private DateOnly _schoolStartDate;
+
+        //właściwości
+        public string StudentName { 
+            get => _studentName; 
+
+            set {
+                if (string.IsNullOrEmpty(value)) {
+                    throw new ArgumentNullException("Imię nie może być puste");
+                    _studentName = value;
+                }
+            } }
+
+
+        public DateOnly BirthDate { 
+            get => _birthDate;
+
+            set {
+                if (value >= DateOnly.FromDateTime(DateTime.Today)) {
+                    throw new ArgumentNullException("Data urodzenia nie moze byc w przyszlosci");
+                    _birthDate = value;
+                }
+            }
+        }
+
+        public int StudentAge {
+            get {
+                int age = DateOnly.FromDataTime(DateTime.Today).Year - _birthDate.Year;
+                if ((DateOnly.FromDateTime(DateTime.Today).Month) < _birthDate.Month ||
+                    (DateOnly.FromDateTime(DateTime.Today).Month) == _birthDate.Month &&
+                    (DateOnly.FromDateTime(DateTime.Today).Month) < _birthDate.Month ) {
+                    age--;
+                }
+                return age;
+            }
+        }
+
+        public Grade StudentGrade {
+            get => _studentGrade;
+
+            set {
+                if (!Enum.IsDefined(typeof(Grade), value))
+                    throw new ArgumentNullException("Nieprawidłowa klasa");
+
+                _studentGrade = value;
+            }
+        }
+
+        public DateOnly SchoolStartDate {
+            get=> _schoolStartDate;
+
+            set {
+                if (value > DateOnly.FormDateTime(DateTime.Today))
+                    throw new ArgumentNullException("Data rozpocęcia nauki nie moze byc w przyszlosci");
+                _schoolStartDate = value;
+            }
+        }
+
+
+    }
+    internal class Program {
+        static void Main(string[] args) {
+
+
+        }
+    }
+}
---
+++ Tykhomyrova/10.03_9.4_konstruktory/10.03_9.4_konstruktory/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("10.03_9.4_konstruktory")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("10.03_9.4_konstruktory")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("28964d33-9e4c-426e-b02d-0f32be6e0887")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
