7d39de6d4b6edab52e6f309f06af62eb26e36a2f
KTykhomyrova <tykhomyrovak@gmail.com>
2025-04-28 11:22:16 +0200

    Add files via upload

---
+++ TaskApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskApp.modele;
+
+namespace TaskApp {
+    internal class Program {
+        static void Main(string[] args) {
+            TaskApp.modele.Task task1 = new TaskApp.modele.Task();
+
+            task1.Title = "ZAdanie 1";
+            task1.Description = "Opis zadania 1";
+            task1.DueDate = DateTime.Now.AddDays(2);
+            task1.IsCompleted = false;
+
+            task1.DisplayInfo();
+
+            TaskApp.modele.Task task2 = new TaskApp.modele.Task("Zadanie 2", "Opis zadania 2", DateTime.Now.AddDays(5), true);
+            task2.DisplayInfo();
+
+            try 
+            {
+                task2.DueDate = DateTime.Now.AddDays(-5);
+            } 
+            catch(ArgumentException ex) 
+            {
+                Console.WriteLine($"Błąd: {ex.Message}");
+            }
+
+            task2.DueDate.AddDays(5);
+            task2.DisplayInfo();
+        }
+    }
+}
